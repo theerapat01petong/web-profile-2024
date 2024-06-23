3776de2e85aade0d1cd122e4b1d0e9ef1078674a
theerapat01petong <theerapat.ch@mail.wu.ac.th>
2024-06-23 10:53:30 +0700

    update.layout.tsx
    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "John Farmer",
+  title: "Theerapat Chotchaung",
   description: "Example personal website",
 };
 
